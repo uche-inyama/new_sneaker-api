1ae65a6d0f5465cfe14a7550c7a01a8890ce1696
uche-inyama <depoulet@yahoo.com>
2023-02-23 11:37:41 -0600

    add rails credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -51,10 +51,10 @@ Rails.application.configure do
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
-
+  
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
@@ -71,10 +71,9 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
-
+  
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
