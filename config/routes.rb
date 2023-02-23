e79c796fdb78dbb2098d022601b751f0c51973b4
uche-inyama <depoulet@yahoo.com>
2023-02-23 13:37:04 -0600

    update route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   devise_for :users, :controllers => { 
     :registrations => "api/v1/registrations",
     :sessions => "api/v1/sessions"
   }
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "api/v1/sessions#new"
+    end
+  end
+
   resources :products
 end
