fd6e84619a93cf8f08531e9a112ac5aef14a6f90
uche-inyama <depoulet@yahoo.com>
2025-06-30 21:23:05 +0100

    update sidekiq initializer file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,10 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
+
+# redis://default:a79bda0a29b6414e902e8799ea04701b@fly-sneaker-api-redis-00.upstash.io:6379
+
