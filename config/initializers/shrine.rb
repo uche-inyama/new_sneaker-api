2fc53d29f357af5afef01723ad9d8ed76f9e5b4a
uche-inyama <depoulet@yahoo.com>
2025-07-01 16:46:48 +0100

    Fix shrine storage initialization timing

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -35,28 +35,28 @@ Shrine.plugin :validation_helpers
 Shrine.plugin :validation
 Shrine.plugin :determine_mime_type 
 
-Rails.application.config.after_initialize do
-  cloud_name    = ENV["CLOUD_NAME"]
-  cloud_api_key = ENV["CLOUD_API_KEY"]
-  cloud_api_secret = ENV["CLOUD_API_SECRET"]
 
-  if cloud_name && cloud_api_key && cloud_api_secret
+cloud_name    = ENV["CLOUD_NAME"]
+cloud_api_key = ENV["CLOUD_API_KEY"]
+cloud_api_secret = ENV["CLOUD_API_SECRET"]
+
+
+if cloud_name && cloud_api_key && cloud_api_secret
+
+  Shrine.storages = {
+    cache: Shrine::Storage::Cloudinary.new(prefix: "cache"),
+    store: Shrine::Storage::Cloudinary.new(prefix: "rails_uploads"),
+  }
+
+  Rails.application.config.after_initialize do
     Cloudinary.config do |config|
       config.cloud_name = cloud_name
       config.api_key = cloud_api_key
       config.api_secret = cloud_api_secret
       config.secure = true
     end
-
-    Shrine.storages = {
-      cache: Shrine::Storage::Cloudinary.new(prefix: "cache"),
-      store: Shrine::Storage::Cloudinary.new(prefix: "rails_uploads"),
-    }
-  else
-    Rails.logger.warn "Cloudinary ENV vars missing. Skipping Cloudinary config."
+      Rails.logger.info "Cloudinary Cloud Name: #{cloud_name}"
   end
-
-
-  Rails.logger.info "Cloudinary Cloud Name: #{cloud_name}"
+else
+  Rails.logger.warn "Cloudinary ENV vars missing. Skipping Cloudinary config."
 end
-
